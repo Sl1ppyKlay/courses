0b9aab19672e9baa4ea17faa5e10321f872d862e
Sergei <103577582+Sl1ppyKlay@users.noreply.github.com>
2023-09-26 23:13:08 +1000

    Update game refactoring.py

--- two_task/game refactoring.py
+++ two_task/game refactoring.py
@@ -1,15 +1,34 @@
 import random
 
 def main():
-    ans = input('Привет!\nБудешь угадывать? (да/нет)').lower()
-    if ans == 'да':
-        play_game()
-    elif ans == 'нет':
-        print('Пока!')
-    else:
+    rand = random.randint(1, 10)
+    ans = input('Привет!\nБудешь угадывать? (да/нет): ').lower()
+
+    if ans != 'да':
         print('Ошибка ввода!')
 
-def play_game():
-    guess = input('Угадай чисто от 1 до 10: ')
-    rand = random.randint(1, 10)
-    
+    if ans == 'нет':
+        print ('Пока')
+
+    while True:
+        try:
+            number = int(input('Угадай чисто от 1 до 10: '))
+            if 1 <= number <= 10:
+                control = abs(number - rand)
+                if control > 5:
+                    print('\nХолодно!\n')
+                elif control > 2:
+                    print ('\nТепло!\n')
+                else:
+                    print ('\nЖгётся!\n')
+                
+                if number == rand:
+                    print('Вы угадали число! Число было -', rand)
+                    break
+            else: 
+                print ('\nЧисло должно быть в пределах от 1 до 10!')
+        except ValueError:
+            print(f'\nОшибка! Должно быть введено число!')
+
+if __name__ == '__main__':
+    main()
