b4b59246025b58f3638a33ccad4d2538a15b8e9e
Sergei <103577582+Sl1ppyKlay@users.noreply.github.com>
2023-09-27 18:09:10 +1000

    Update student-2023-assignment.py

--- one_task/student-2023-assignment.py
+++ one_task/student-2023-assignment.py
@@ -11,20 +11,19 @@ def open_file(path): # открытие файла
         return json.load(r)
 
 
-def dictionary(): # словарь
+def dictionary(f_path): # словарь
     uniq_replacement = {}
 
-    for replacement in reversed(open_file('replacement.json')):
+    for replacement in reversed(open_file(f_path)):
         replacement['source'] = replacement['source'] or ""  # Замена none на пустую строку
         uniq_replacement.setdefault(replacement['replacement'], replacement)
 
     return uniq_replacement
 
 
-def source_text(): # получение исходного текста
-    data = api('https://raw.githubusercontent.com/thewhitesoft/student-2023-assignment/main/data.json')
+def source_text(data, f_path): # получение исходного текста
 
-    uniq_replacement = dictionary()
+    uniq_replacement = dictionary(f_path)
     updated_data = []
 
     for item in data:
@@ -35,9 +34,9 @@ def source_text(): # получение исходного текста
     return updated_data
 
 
-def save_source_text(): # сохранения исходного текста
-    updated_data = source_text()
+def save_source_text(): # сохранение исходного текста
     lines_list = []
+    updated_data = source_text(data, f_path)
 
     for item in updated_data:
         lines_list.extend([line for line in item.split('\n') if line.strip()]) # удаление пустых строк
@@ -45,10 +44,18 @@ def save_source_text(): # сохранения исходного текста
     with open('result.json', 'w') as result:
         try:
             json.dump(lines_list, result, ensure_ascii=False, indent=4)
-            print ('Текст успешно восстановлен и сохранен!')
+            print ('\nТекст успешно восстановлен и сохранен!')
         except Exception as e:
             print (f'Ошибка - {e}')
 
+def data_input():
+    try:
+        data = api(input('Привет!\nНапиши ссылку для парсинга данных с сайта: '))
+        f_path = str(input('Напиши путь до json файла: '))
+    except ValueError:
+        print ('Произошла ошибка!')
+    return data, f_path
 
 if __name__ == '__main__':
+    data, f_path = data_input()
     save_source_text()
